6cbcaf7fd268aa2a2bd8a6df916346d020eed157
Andrew Sauder <andrewsauder@gmail.com>
2024-05-08 11:55:36 -0400

    move item interface

--- components/UpcLookup.vue
+++ components/UpcLookup.vue
@@ -1,41 +1,13 @@
 <script lang="ts" setup>
-import { ref } from 'vue';
-import ObjectID from "bson-objectid";
-
-/*defineProps({
-  msg: String,
-});*/
+import {ref, Ref} from 'vue';
+import {IItem} from "@/components/upcLookup";
 
 const upc: Ref<string> = ref('')
-const data: Ref<object> = ref({})
+const data: Ref<IItem|null> = ref(null)
 
-interface IItem {
-    category:string|undefined,
-	description:string|undefined,
-	ean:string|undefined,
-    upc:string|undefined,
-	images:string[]|undefined,
-    title:string[]|undefined,
-    //book
-    isbn:string|undefined,
-    publisher:string[]|undefined,
-	//product
-	brand:string|undefined,
-	color:string|undefined,
-	currency:string|undefined,
-	dimension:string|undefined,
-	highest_recorded_price:number|undefined,
-	lowest_recorded_price:number|undefined,
-	model:string|undefined,
-	size:string|undefined,
-	weight:string|undefined,
-}
 const lookup = async () => {
 	console.log('Lookup: '+upc.value)
-    let rsp = await upcLookup.lookupUpc(upc.value)
-
-	console.log(rsp.data)
-    data.value = rsp.data
+    data.value = await upcLookup.lookupUpc(upc.value)
 
 }
 </script>
--- components/upcLookup.ts
+++ components/upcLookup.ts
@@ -1,3 +1,24 @@
+export interface IItem {
+    category:string|undefined,
+        description:string|undefined,
+        ean:string|undefined,
+        upc:string|undefined,
+        images:string[]|undefined,
+        title:string[]|undefined,
+        //book
+        isbn:string|undefined,
+        publisher:string[]|undefined,
+        //product
+        brand:string|undefined,
+        color:string|undefined,
+        currency:string|undefined,
+        dimension:string|undefined,
+        highest_recorded_price:number|undefined,
+        lowest_recorded_price:number|undefined,
+        model:string|undefined,
+        size:string|undefined,
+        weight:string|undefined,
+}
 export default class upcLookup {
 
     public static baseUrl:string = 'https://api.upcitemdb.com/prod/trial/'
@@ -5,7 +26,7 @@ export default class upcLookup {
     constructor() {
     }
 
-    static lookupUpc = async ( upc:string='') => {
+    static lookupUpc = async ( upc:string=''): Promise<IItem|null> => {
        const response = await fetch(upcLookup.baseUrl+'lookup?upc='+upc );
        const body = await response.json();
        if(body.code=='OK' && body.items[0]) {
